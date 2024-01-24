044f6a3ddd29864b921c1c55613643e9760cb362
Carlos Sosa <isek7or@gmail.com>
2024-01-23 20:19:32 -0800

    completed all routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Animal from "./Components/Animal";
 import NewAnimal from "./Components/NewAnimal";
+import UpdateAnimal from "./Components/UpdateAnimal";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/animal/:id' element={<Animal />} />
           <Route path='/animal/new' element={<NewAnimal />} />
+          <Route path='/animal/update/:id' element={<UpdateAnimal />} />
         </Routes>
       </Router>
     </div>
